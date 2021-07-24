3b0f8be7166157759cc5da07da3de4bc8c1018ba
Malavika <malavikau1801@gmail.com>
2021-07-25 00:38:03 +0530

    lomutos partition, swap function, quick sort added

--- randomizedPivot.c
+++ randomizedPivot.c
@@ -1,4 +1,40 @@
+//randomized quick sort
+//randomly generated pivot further improves time complexity
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(){}
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partitionHoares(int a[], int low, int high){
+    int pivot = a[high];
+    int i, j;
+
+    i = low; 
+
+    for(j = low; j < high ; ++j){
+        if(a[j] <= pivot){
+            swap(a[i], a[j]);
+            ++i;
+        }
+    }
+
+    swap(a[i], a[high]);
+
+    return i;
+}
+
+void quickSort(int a[], int low, int high, ){
+    if (low < high){
+        int pivot = partitionLomuto(arr, low, high);
+
+        quickSort(a, low, pivot);
+        quickSort(a, pivot + 1, high);
+    }
+}
+int main(){
+    
+}
