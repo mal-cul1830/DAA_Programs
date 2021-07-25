2ef03da72cda39c714bdf09f1f87a40b2a423664
Malavika <malavikau1801@gmail.com>
2021-07-25 16:14:07 +0530

    adding some utility fnts, for shell sort

---
+++ shellSort.c
@@ -0,0 +1,42 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void print_array(int a[], int n, char *message){
+    if(message!=NULL)
+        printf(message);
+
+    int i; 
+
+    for(i = 0; i< n-1 ; ++i)
+        printf("%d , ", a[i]);
+    printf("%d\n", a[i]);
+}
+
+void read_array(int a[], int n, char *message){
+    if(message!=NULL)
+        printf(message);
+    for(int i = 0; i<n ; ++i){
+        scanf("%d", &a[i]);
+        //printf("read %d, %d \n", a[i], i);
+    }
+}
+
+void copy_array(int source[], int destination[], int n){
+    int i;
+    for(i = 0; i < n ; ++i)
+        destination[i] = source[i];
+
+    printf("Array copy done. \n");
+}
+
+int gapCalc(int n, int d){
+    int gap = floor(n/pow(2, d));
+    return gap;
+}
